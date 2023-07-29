e674f46b538a640620398a1e19b46687bf2322e3
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-29 13:32:19 -0500

    add logo for mobile and tablet size

--- src/components/Navigation/TopNavigation.tsx
+++ src/components/Navigation/TopNavigation.tsx
@@ -1,5 +1,20 @@
+import { useMediaQuery } from "react-responsive";
+
+import logoLight from "/logo-light.svg";
+
 const TopNavigation = () => {
-  return <h1>Hello from Top Nav</h1>;
+  const isTabletOrMobile = useMediaQuery({ minWidth: 768 });
+  return (
+    <div className="flex items-center justify-center py-6 border-b-2">
+      {/* {FIXME: Convert to react router link} */}
+      {!isTabletOrMobile && (
+        <a href="">
+          <img src={logoLight} alt="cook.io home" />
+        </a>
+      )}
+      {isTabletOrMobile && <p>show at screen size greater than 768</p>}
+    </div>
+  );
 };
 
 export default TopNavigation;
