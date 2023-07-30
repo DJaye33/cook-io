25a7a3b9511577619db2099814446affba7e8203
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-30 00:06:34 -0500

    add header input field component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,10 @@
+import HeaderInputGroup from "../HeaderInputGroup.tsx/HeaderInputGroup";
+
 const Header = () => {
   return (
-    <header className="bg-[url('../src/assets/hero-banner-small.jpg')] bg-cover bg-no-repeat bg-center h-[480px] md:bg-[url('../src/assets/hero-banner-medium.jpg')] xl:bg-[url('../src/assets/hero-banner-large.jpg')]"></header>
+    <header className="p-5 bg-[url('../src/assets/hero-banner-small.jpg')] bg-cover bg-no-repeat bg-center h-[480px] md:bg-[url('../src/assets/hero-banner-medium.jpg')] xl:bg-[url('../src/assets/hero-banner-large.jpg')]">
+      <HeaderInputGroup />
+    </header>
   );
 };
 
